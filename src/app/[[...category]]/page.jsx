3e6925597887e588b78b6f239d73385ddbf654d3
Nazim Rzayev <nazimrzayev527@gmail.com>
2024-02-06 20:10:05 +0400

    Categories section add

---
+++ src/app/[[...category]]/page.jsx
@@ -0,0 +1,16 @@
+import HomeContainer from "@/containers/Home";
+import Link from "next/link";
+import Movies from '@/mocks/movies.json'
+
+export default function Home({ params }) {
+  let selectedCategory;
+
+  if(params.category?.length > 0) {
+    selectedCategory = true
+  }
+  return <HomeContainer 
+  selectedCategory = {{
+    id: params.category?.[0] ?? "",
+    movies: selectedCategory ? Movies.results.slice(0,7) : []
+  }}/>;
+}
--- src/app/page.js
+++
@@ -1,9 +0,0 @@
-import HomeContainer from "@/containers/Home";
-import Link from "next/link";
-
-export default function Home() {
-  return (
-    <HomeContainer/>
-  );
-}
- 
